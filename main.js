b0e43c0733c7c9f7d4372e11b3e7bbbaab946fd7
ZiadAlsaedi <ziad12399@gmail.com>
2024-05-27 15:01:36 +0300

    v1

--- main.js
+++ main.js
@@ -1,107 +1,156 @@
-function mOver() {
-document.getElementById('name').textContent = 'Ziad Alsaedi';
-document.getElementById('role').textContent = 'FRONT END DEVLOPER';
-document.getElementById('aboutMe').textContent = `
-Ziad Alsaedi, a Computer Information Systems graduate from Taibah University in 2023, is passionate about software development and web technologies. Experienced in web application development and databases, I’m always eager to learn new technologies and implement them in projects. I aim to join an innovative and dynamic team where I can further develop my skills and achieve my professional goals.
-`;
-document.getElementById('company-name').textContent = 'Taibah University ';
-document.getElementById('titleTraning').textContent = 'TRAINING ';
-document.getElementById('time').textContent = 'Jun.2022 – Aug.2022 ';
-document.getElementById('job').textContent = 'System Analyst';
-document.getElementById('point').textContent = 'Analysis the system of equivalence of academic courses and design User interface.';
-document.getElementById('point2').textContent = 'Working on designing interfaces for university systems.';
-document.getElementById('point3').textContent = 'Converting interface design into HTML pages.';
-
-document.getElementById('company-name2').textContent = 'Tuwaiq Academy';
-document.getElementById('time2').textContent = 'Apr.2024 – Jul.2024 ';
-document.getElementById('job2').textContent = 'JavaScript Devloper';
-document.getElementById('point4').textContent = 'Learn to design web pages by Figma.';
-document.getElementById('point5').textContent = 'Creating projects using HTML, CSS, and Bootstrap.';
-document.getElementById('point6').textContent = 'Learn JavaScript and how to interact with websites.';
-
-document.getElementById('Rname').textContent = 'Dr. Muhannad ';
-document.getElementById('Rname2').textContent = 'Dr. Ayman';
-document.getElementById('Rjob').textContent = 'Doctor at Taibah University';
-document.getElementById('Rjob2').textContent = 'Doctor at Taibah University';
-document.getElementById('Rphone').textContent = 'phone: 0546444481';
-document.getElementById('Rphone2').textContent = 'phone: 0546534931';
-document.getElementById('Remail').textContent = 'email:  Muhannad.gmail.com';
-document.getElementById('Remail2').textContent = 'email:  Ayman.gmail.com';
-
-document.getElementById('phone').textContent = 'Phone Number: 0534834445';
-document.getElementById('email').textContent = 'Email: Ziad12399.gmail.com';
-document.getElementById('linkedin').textContent = 'Account: https://www.linkedin.com/in/ziad-alsaedi-4671841b7/';
-document.getElementById('street').textContent = 'Address: DMW 3085, 7104 Al-Hijra Branch Road, Al-Qaswa, Medina';
-document.getElementById('eYear').textContent = '2018 - 2023';
-document.getElementById('UNVERSITY').textContent = 'TAIBAH UNVERSITY';
-document.getElementById('major').textContent = 'information system';
-
-document.getElementById('point7').textContent = 'Responsibility';
-document.getElementById('point8').textContent = 'Self-Learning.';
-document.getElementById('point9').textContent = 'Collaboration';
-document.getElementById('point10').textContent = 'Flexibility';
-document.getElementById('point11').textContent = 'Work with group';
-document.getElementById('point12').textContent = 'Problem Solving';
-document.getElementById('point13').textContent = 'Time management';
-
-document.getElementById('Lang1').textContent = 'Arabic(Fluent) ';
-document.getElementById('Lang2').textContent = 'English(intermediate) ';
-document.getElementById('image').src = './صورة واتساب بتاريخ 1445-11-06 في 00.29.21_f880be32.jpg';
-
-document.getElementById('point14').textContent = 'HTML & CSS';
-document.getElementById('point15').textContent = 'Java Script';
-document.getElementById('point16').textContent = 'System Analysis';
-document.getElementById('point17').textContent = 'SQL';
-document.getElementById('point18').textContent = 'Django';
-document.getElementById('point19').textContent = 'Figma';
-document.getElementById('point20').textContent = 'Bootstrap';
-
-document.getElementById('cer1').textContent = `Programming Mobile Applications for Android Handheld Systems (8 weeks), Coursera, Online, 
-Dec.2022 – Mar.2023.`;
-
-document.getElementById('cer2').textContent = `Mobile Computing Applications (8 weeks), Future X, Online, Dec.2022 – Mar.2023.`;
-
-document.getElementById('cer3').textContent = `Creating Web Applications Using The Django Framework (60 hours),Tuwaiq Academy, Online,
-Feb.2024-Mar.2024.
-`;
 
 
+function phone() {
+    document.getElementById('phone').textContent = 'Phone Number: 0534834445';
+}
+function email() {
+    document.getElementById('email').textContent = 'Email: Ziad12399.gmail.com';
+}
+
+function account() {
+    document.getElementById('linkedin').textContent = 'Account: https://www.linkedin.com/in/ziad-alsaedi-4671841b7/';
+}
+function Address() {
+    document.getElementById('street').textContent = 'Address: DMW 3085, 7104 Al-Hijra Branch Road, Al-Qaswa, Medina';
+}
+function EDUCATION() {
+
+    document.getElementById('eYear').textContent = '2018 - 2023';
+    document.getElementById('UNVERSITY').textContent = 'TAIBAH UNVERSITY';
+    document.getElementById('major').textContent = 'information system';
+    
+}
+
+function SoftSkills() {
+
+    document.getElementById('point7').textContent = 'Responsibility';
+    document.getElementById('point8').textContent = 'Self-Learning.';
+    document.getElementById('point9').textContent = 'Collaboration';
+    document.getElementById('point10').textContent = 'Flexibility';
+    document.getElementById('point11').textContent = 'Work with group';
+    document.getElementById('point12').textContent = 'Problem Solving';
+    document.getElementById('point13').textContent = 'Time management';
+    
+} 
+function TechnicalSkills() {
+
+    document.getElementById('point14').textContent = 'HTML & CSS';
+    document.getElementById('point15').textContent = 'Java Script';
+    document.getElementById('point16').textContent = 'System Analysis';
+    document.getElementById('point17').textContent = 'SQL';
+    document.getElementById('point18').textContent = 'Django';
+    document.getElementById('point19').textContent = 'Figma';
+    document.getElementById('point20').textContent = 'Bootstrap';
+
+}
+
+function LANGUAGES() {
+    document.getElementById('Lang1').textContent = 'Arabic(Fluent) ';
+    document.getElementById('Lang2').textContent = 'English(intermediate) ';
+}
+
+function photo() {
+    document.getElementById('image').src = './صورة واتساب بتاريخ 1445-11-06 في 00.29.21_f880be32.jpg';
+
+}
 
 
+function CERTIFICATIONS() {
 
-
+    document.getElementById('cer1').textContent = `Programming Mobile Applications for Android Handheld Systems (8 weeks), Coursera, Online, 
+    Dec.2022 – Mar.2023.`;
+    
+    document.getElementById('cer2').textContent = `Mobile Computing Applications (8 weeks), Future X, Online, Dec.2022 – Mar.2023.`;
+    
+    document.getElementById('cer3').textContent = `Creating Web Applications Using The Django Framework (60 hours),Tuwaiq Academy, Online,
+    Feb.2024-Mar.2024.
+    `;
+    
+    
+}
 
 
 
+function mOvername() {
+    document.getElementById('name').textContent = 'Ziad Alsaedi';
+    document.getElementById('role').textContent = 'FRONT END DEVLOPER';
+}
+
+function mOverProfile() {
+    document.getElementById('aboutMe').textContent = `
+    Ziad Alsaedi, a Computer Information Systems graduate from Taibah University in 2023, is passionate about software development and web technologies. Experienced in web application development and databases, I’m always eager to learn new technologies and implement them in projects. I aim to join an innovative and dynamic team where I can further develop my skills and achieve my professional goals.
+    `;
+}
 
 
 
+function EXPERINES1() {
+    document.getElementById('company-name').textContent = 'Taibah University ';
+    document.getElementById('titleTraning').textContent = 'TRAINING ';
+    document.getElementById('time').textContent = 'Jun.2022 – Aug.2022 ';
+    document.getElementById('job').textContent = 'System Analyst';
+    document.getElementById('point').textContent = 'Analysis the system of equivalence of academic courses and design User interface.';
+    document.getElementById('point2').textContent = 'Working on designing interfaces for university systems.';
+    document.getElementById('point3').textContent = 'Converting interface design into HTML pages.';
+    
+}
 
+function EXPERINES2() {
 
+    document.getElementById('company-name2').textContent = 'Tuwaiq Academy';
+    document.getElementById('time2').textContent = 'Apr.2024 – Jul.2024 ';
+    document.getElementById('job2').textContent = 'JavaScript Devloper';
+    document.getElementById('point4').textContent = 'Learn to design web pages by Figma.';
+    document.getElementById('point5').textContent = 'Creating projects using HTML, CSS, and Bootstrap.';
+    document.getElementById('point6').textContent = 'Learn JavaScript and how to interact with websites.';
+    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function REFRENCE() {
+    document.getElementById('Rname').textContent = 'Dr. Muhannad ';
+    document.getElementById('Rjob').textContent = 'Doctor at Taibah University';
+    document.getElementById('Rphone').textContent = 'phone: 0546444481';
+    document.getElementById('Remail').textContent = 'email:  Muhannad.gmail.com';
 
 
 
 }
+function REFRENCE2() {
+    document.getElementById('Rname2').textContent = 'Dr. Ayman';
+    document.getElementById('Rjob2').textContent = 'Doctor at Taibah University';
+    document.getElementById('Rphone2').textContent = 'phone: 0546534931';
+    document.getElementById('Remail2').textContent = 'email:  Ayman.gmail.com';
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
